3a4fbce44c059d5228ce30741ab42bd6c5dee26e
R4DEONN <vipzulus1@mail.ru>
2023-10-06 11:14:32 +0300

    Провел через линтер

--- mockObjectsMin.ts
+++ mockObjectsMin.ts
@@ -1,46 +1,39 @@
 import * as Type from './types'
 
 const WhiteColor: Type.Color = {
-    hex: "#FFFFFF",
-    opacity: 0,
+	hex: '#FFFFFF',
+	opacity: 0,
 }
 
 const background: Type.Background = {
-    color: {
-        colors: [WhiteColor],
-    },
+	color: {
+		colors: [WhiteColor],
+	},
 }
 
 const slide: Type.Slide = {
-    id: "DHASDH7585dASD",
-    background,
-    objects: [],
+	id: 'DHASDH7585dASD',
+	background,
+	objects: [],
 }
 
 const presentation: Type.Presentation = {
-    title: "Min presentation",
-    slides: [slide],
+	title: 'Min presentation',
+	slides: [slide],
 }
 
 const selection: Type.Selection = {
-    slideId: "DHASDH7585dASD",
+	slideId: 'DHASDH7585dASD',
 }
 
 const operationHistory: Type.OperationHistory = {
-    operations: [],
+	operations: [],
 }
 
 const presenter: Type.Presenter = {
-    presentation,
-    selection,
-    operationHistory,
+	presentation,
+	selection,
+	operationHistory,
 }
 
-export {
-    presenter,
-    operationHistory,
-    selection,
-    presentation,
-    slide,
-    background,
-}
+export { presenter, operationHistory, selection, presentation, slide, background }
