0c06cc6fa5c5ff2f5faad9d8325c9af5c12d3475
Gregogo <87802379+Gregogo@users.noreply.github.com>
2023-10-21 13:18:53 +0300

    HOT-FIX многа

--- src/editor/WorkSpace/WorkSpace.tsx
+++ src/editor/WorkSpace/WorkSpace.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 import './WorkSpace.css'
 import { BookMarks } from './bookMarks/BookMarks'
 import { SlideEditor } from './SlideEditor/SlideEditor'
+import { SlideList } from './slideList/SlideList'
 
 function WorkSpace() {
 	return (
 		<div className="WorkSpace">
-			<BookMarks></BookMarks>
-			<SlideEditor></SlideEditor>
+			<div>
+				<BookMarks></BookMarks>
+				<div className="slide-editor-wrapper">
+					<SlideEditor></SlideEditor>
+				</div>
+			</div>
+			<SlideList></SlideList>
 		</div>
 	)
 }
---
+++ src/editor/WorkSpace/slideList/SlideList.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './SlideList.css'
+
+function SlideList() {
+	return <div className="slide-list"></div>
+}
+
+export { SlideList }
