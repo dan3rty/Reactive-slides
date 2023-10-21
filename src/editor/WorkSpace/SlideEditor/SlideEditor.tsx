b34e3e08ae71403d451cfb0f91e55cb8d65bf27d
Gregogo <87802379+Gregogo@users.noreply.github.com>
2023-10-21 12:36:21 +0300

    HOT-FIX многа

---
+++ src/editor/WorkSpace/SlideEditor/SlideEditor.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './SlideEditor.css'
+
+function SlideEditor() {
+	return <div className="slide-editor"></div>
+}
+
+export { SlideEditor }
--- src/editor/WorkSpace/WorkSpace.tsx
+++ src/editor/WorkSpace/WorkSpace.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './WorkSpace.css'
 import { BookMarks } from './bookMarks/BookMarks'
+import { SlideEditor } from './SlideEditor/SlideEditor'
 
 function WorkSpace() {
 	return (
 		<div className="WorkSpace">
 			<BookMarks></BookMarks>
+			<SlideEditor></SlideEditor>
 		</div>
 	)
 }
