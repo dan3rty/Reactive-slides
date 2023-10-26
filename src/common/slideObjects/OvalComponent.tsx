accaa011e34b1002f4b8af6e3c738fa97aa5050e
Gregogo <87802379+Gregogo@users.noreply.github.com>
2023-10-26 14:09:21 +0300

    HOT-FIX многа

--- src/common/slideObjects/OvalComponent.tsx
+++ src/common/slideObjects/OvalComponent.tsx
@@ -1,31 +1,29 @@
-import React, {ReactNode, useContext} from 'react'
+import React, { ReactNode, useContext } from 'react'
 import './OvalComponent.css'
-import {GradientColor, PrimitiveBlock} from "../../types";
+import { GradientColor, PrimitiveBlock } from '../../types'
 
 type OvalProps = {
-    oval: PrimitiveBlock
+	oval: PrimitiveBlock
 }
 
-function returnGradientString (colors: GradientColor) {
-    let string = colors.colors[0].hex
-    for (let i = 1; i < colors.colors.length; i++) {
-        string = string + ', ' + colors.colors[i].hex
-    }
-    return 'linear-gradient(' + string + ')'
+function returnGradientString(colors: GradientColor) {
+	let string = colors.colors[0].hex
+	for (let i = 1; i < colors.colors.length; i++) {
+		string = string + ', ' + colors.colors[i].hex
+	}
+	return 'linear-gradient(' + string + ')'
 }
 
 function OvalComponent(props: OvalProps) {
-    const ovalStyle = {
-        position: 'absolure',
-        background: returnGradientString(props.oval.color),
-        width: props.oval.baseState.width + 'px',
-        height: props.oval.baseState.height + 'px',
-        top: props.oval.baseState.y + 'px',
-        bottom: props.oval.baseState.x + 'px',
-    }
-    return(
-        <div style={ovalStyle}></div>
-    )
+	const ovalStyle = {
+		position: 'absolure',
+		background: returnGradientString(props.oval.color),
+		width: props.oval.baseState.width + 'px',
+		height: props.oval.baseState.height + 'px',
+		top: props.oval.baseState.y + 'px',
+		bottom: props.oval.baseState.x + 'px',
+	}
+	return <div style={ovalStyle}></div>
 }
 
 export { OvalComponent }
--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import '../common/styles/App.css'
 import { ToolBar } from './toolBar/ToolBar'
 import { WorkSpace } from './WorkSpace/WorkSpace'
 import { Tabs } from '../types'
+import { presentation } from '../App'
 
 const chosenTab = React.createContext(Tabs.CREATE)
 function Editor() {
+	const selectedTab = useContext(presentation).selection.selectedTab
 	return (
-		<chosenTab.Provider value={Tabs.EDIT}>
+		<chosenTab.Provider value={selectedTab}>
 			<div className='App'>
 				<ToolBar></ToolBar>
 				<WorkSpace></WorkSpace>
