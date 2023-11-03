9ace84ea609db84d6cbe568f75a3b7fb7b364575
Gregogo <87802379+Gregogo@users.noreply.github.com>
2023-11-03 10:03:18 +0300

    AAAA

--- src/editor/WorkSpace/SlideEditor/SlideEditor.tsx
+++ src/editor/WorkSpace/SlideEditor/SlideEditor.tsx
@@ -1,15 +1,25 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import './SlideEditor.css'
-import {presentation} from "../../../App";
-import {TextComponent} from "../../../common/slideObjects/TextComponent";
-import {text} from "../../../mockObjects";
+import { presentation } from '../../../App'
+import { TextComponent } from '../../../common/slideObjects/TextComponent'
+import { BlockType } from '../../../types'
 
 function SlideEditor() {
 	const chosen = useContext(presentation).selection.slideId
-	const curSlide = useContext(presentation).presentation.slides.find((slide) => slide.id === chosen)
-	return <div className='slide-editor'>
-		{TextComponent({text})}
-	</div>
+	const curSlide = useContext(presentation).presentation.slides.find(
+		(slide) => slide.id === chosen,
+	)
+	if (curSlide) {
+		const slideObjects = curSlide.objects
+		const objectsToRender = slideObjects.map((object) => {
+			if (object.blockType === BlockType.TEXT) {
+				return <TextComponent text={object}></TextComponent>
+			}
+			return <div></div>
+		})
+		return <div className='slide-editor'>{objectsToRender}</div>
+	}
+	return <div></div>
 }
 
 export { SlideEditor }
