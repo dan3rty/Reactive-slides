d552b4426ca92287711320944d550d9ba6b9b7c9
Gregogo <TI@>
2023-11-17 09:36:19 +0300

    fhfghf

--- src/mockObjects.ts
+++ src/mockObjects.ts
@@ -447,7 +447,7 @@ const presentation: Type.Presentation = {
 }
 
 const selection: Type.Selection = {
-	selectedTab: Type.Tabs.ANIMATION,
+	selectedTab: Type.Tabs.EDIT,
 	slideId: 'djahDHSADGKUAGUSDAD',
 	objectsId: ['D5DAS67F5A'],
 	keyFrameId: 'polka',
