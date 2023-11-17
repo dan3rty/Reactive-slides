082de76353036418990eeea1e9ccc76854d3af01
dan3rty <dan3rty@yandex.ru>
2023-11-17 13:06:12 +0300

    Правки по ревью

--- src/editor/toolBar/settingBar/animationBar/AnimationBar.tsx
+++ src/editor/toolBar/settingBar/animationBar/AnimationBar.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { InputField } from '../../../../common/components/inputFields/InputField'
 import styles from './AnimationBar.module.css'
 import { Button } from '../../../../common/components/buttons/Button'
 import { Timeline } from './timeline/Timeline'
+import { presentation } from '../../../../App'
 
 function AnimationBar() {
+	const chosenSlide = useContext(presentation).selection.slideId
+	const chosenObjectId = useContext(presentation).selection.objectsId
+	const chosenState = useContext(presentation).selection.keyFrameId
+	const curSlide = useContext(presentation).presentation.slides.find(
+		(slide) => slide.id === chosenSlide,
+	)
+	const slideObjects = curSlide?.objects
+	const chosenObject = slideObjects?.find((object) => object.id === chosenObjectId[0])
+	const animation = chosenObject?.animation
 	return (
 		<div className={styles.animationBar}>
 			<div className={styles.bigContainer}>
@@ -58,7 +68,7 @@ function AnimationBar() {
 					<Button text={'add keyframe'} style={'dark'} size={'large'} />
 					<Button text={'delete keyframe'} style={'dark'} size={'large'} />
 				</div>
-				<Timeline />
+				<Timeline animation={animation} chosenState={chosenState}></Timeline>
 			</div>
 		</div>
 	)
--- src/editor/toolBar/settingBar/animationBar/timeline/Timeline.tsx
+++ src/editor/toolBar/settingBar/animationBar/timeline/Timeline.tsx
@@ -1,66 +1,39 @@
-import React, { useContext } from 'react'
+import React from 'react'
+import { ObjectStateList } from '../../../../../types'
+import { Dot } from './dot/Dot'
 import styles from './Timeline.module.css'
-import { presentation } from '../../../../../App'
+import './dot/Dot'
 
-function Timeline() {
-	//TODO: сделать константу с useContext(presentation).selection, ВСЕ ВЫТАЩИТЬ В ANIMATIONBAR
-	const chosenSlide = useContext(presentation).selection.slideId
-	const chosenObject = useContext(presentation).selection.objectsId
-	const chosenState = useContext(presentation).selection.keyFrameId
-	const curSlide = useContext(presentation).presentation.slides.find(
-		(slide) => slide.id === chosenSlide,
+type Timeline = {
+	animation: ObjectStateList
+	chosenState: string
+}
+
+function Timeline(props: Timeline) {
+	const { animation, chosenState } = props
+	const lineWidth = 500
+	let objectsCounter = 0
+	const objectsToRender = animation.stateList.map((state) => {
+		const percent = state.keyPercent / 100
+		const duration = animation.duration * percent
+		objectsCounter++
+		const offset = lineWidth * percent - objectsCounter * 40 - 20 + 'px'
+		const isChosen = state.id === chosenState
+		return { duration, offset, isChosen }
+	})
+	const isFirstChosen =
+		chosenState === '' ? styles.timelineLineDotCircleChosen : styles.timelineLineDotCircle
+	return (
+		<div className={styles.timelineContainer}>
+			<div className={styles.timelineLine}>
+				<Dot duration={0} offset={'-20px'} isChosen={isFirstChosen}></Dot>
+				{objectsToRender.map((circle) => {
+					const { duration, offset, isChosen } = circle
+					return <Dot duration={duration} offset={offset} isChosen={isChosen}></Dot>
+				})}
+			</div>
+		</div>
 	)
-	if (curSlide && chosenObject) {
-		const slideObjects = curSlide.objects
-		const curObject = slideObjects.find((object) => object.id === chosenObject[0])
-		const lineWidth = 500
-		let objectsCounter = 0
-		if (curObject && curObject.animation) {
-			const duration = curObject.animation.duration
-			const objectsToRender = curObject.animation.stateList.map((state) => {
-				objectsCounter++
-				const percent = state.keyPercent / 100
-				const offset = objectsCounter * 40 + 20
-				const circle =
-					state.id === chosenState ? (
-						<div className={styles.timelineLineDotCircleChosen}></div>
-					) : (
-						<div className={styles.timelineLineDotCircle}></div>
-					)
-				//TODO ВЫНЕСТИ В CONST lineWidth * percent - offset + 'px'
-				return (
-					<div
-						className={styles.timelineLineDot}
-						style={{ left: lineWidth * percent - offset + 'px' }}
-					>
-						{circle}
-						<span className={styles.timelineLineDotText}>
-							{(duration * percent).toFixed(1)}
-						</span>
-					</div>
-				)
-			})
-			const circle =
-				chosenState === '' ? (
-					<div className={styles.timelineLineDotCircleChosen}></div>
-				) : (
-					<div className={styles.timelineLineDotCircle}></div>
-				)
-			return (
-				<div className={styles.timelineContainer}>
-					<div className={styles.timelineLine}>
-						<div className={styles.timelineLineDot} style={{ left: -20 + 'px' }}>
-							{circle}
-							<span className={styles.timelineLineDotText}>0.0</span>
-						</div>
-						{objectsToRender}
-					</div>
-				</div>
-			)
-		}
-		return <div></div>
-	}
-	return <div></div>
 }
 
 export { Timeline }
