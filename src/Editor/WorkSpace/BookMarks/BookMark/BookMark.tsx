0eb20dbe11a6e3c05b053cf07d325a7b6a46cb3a
kirill.borisov <kirill.borisov@ispring.com>
2023-11-29 22:09:34 +0300

    Добавил выбор вкладок

--- src/Editor/WorkSpace/BookMarks/BookMark/BookMark.tsx
+++ src/Editor/WorkSpace/BookMarks/BookMark/BookMark.tsx
@@ -1,28 +1,23 @@
 import styles from './BookMark.module.css'
-import { ReactNode, useContext } from 'react'
-import { PresenterContext } from '../../../../App'
+import { ReactNode } from 'react'
+import { joinCssClasses } from '../../../../classes/joinCssClasses'
 
 type BookMarkProps = {
 	text: string
 	icon: ReactNode
+	isChosen: boolean
+	onClick: () => void
 }
-function BookMark(props: BookMarkProps) {
-	const curChosen = useContext(PresenterContext).presenter.selection.selectedTab
-	if (curChosen == props.text) {
-		return (
-			<div className={styles.BookMarkChosen}>
-				{props.icon}
-				<span> {props.text} </span>
-			</div>
-		)
-	} else {
-		return (
-			<div className={styles.BookMark}>
-				{props.icon}
-				<span> {props.text} </span>
-			</div>
-		)
-	}
+function BookMark({ text, icon, isChosen, onClick }: BookMarkProps) {
+	return (
+		<div
+			className={joinCssClasses(styles.BookMark, isChosen ? styles.BookMarkChosen : false)}
+			onClick={onClick}
+		>
+			{icon}
+			<span> {text} </span>
+		</div>
+	)
 }
 
 export { BookMark }
--- src/Editor/WorkSpace/BookMarks/BookMarks.tsx
+++ src/Editor/WorkSpace/BookMarks/BookMarks.tsx
@@ -1,14 +1,47 @@
 import styles from './BookMarks.module.css'
 import { BookMark } from './BookMark/BookMark'
 
-import { ArrowIcon, SelectIcon, MoveIcon } from '../../../common/Icons/icons'
+import { ArrowIcon, MoveIcon, SelectIcon } from '../../../common/Icons/icons'
+import { useContext, useState } from 'react'
+import { Selection, Tabs } from '../../../types'
+import { PresenterContext } from '../../../presenterContext/PresenterContext'
 
 function BookMarks() {
+	const [chosen, setChosen] = useState(Tabs.CREATE)
+	const { presentation, selection, operationHistory } = useContext(PresenterContext).presenter
+	const { setPresenter } = useContext(PresenterContext)
+
+	const createOnClick = (tab: Tabs) => {
+		return () => {
+			setChosen(tab)
+			const newSelection: Selection = {
+				...selection,
+				selectedTab: tab,
+			}
+			console.log({ presentation, newSelection, operationHistory })
+			setPresenter({ presentation, selection: newSelection, operationHistory })
+		}
+	}
 	return (
 		<div className={styles.BookMarks}>
-			<BookMark text='create' icon={ArrowIcon}></BookMark>
-			<BookMark text='edit' icon={SelectIcon}></BookMark>
-			<BookMark text='animation' icon={MoveIcon}></BookMark>
+			<BookMark
+				text='create'
+				icon={ArrowIcon}
+				isChosen={chosen == Tabs.CREATE}
+				onClick={createOnClick(Tabs.CREATE)}
+			></BookMark>
+			<BookMark
+				text='edit'
+				icon={SelectIcon}
+				isChosen={chosen == Tabs.EDIT}
+				onClick={createOnClick(Tabs.EDIT)}
+			></BookMark>
+			<BookMark
+				text='animation'
+				icon={MoveIcon}
+				isChosen={chosen == Tabs.ANIMATION}
+				onClick={createOnClick(Tabs.ANIMATION)}
+			></BookMark>
 		</div>
 	)
 }
