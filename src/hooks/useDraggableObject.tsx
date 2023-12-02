51bd754997d293fb79e30fabfdf92d13d4cb7499
poshn <poshnagovgrego@gmail.com>
2023-12-02 19:47:07 +0300

    испрваил днд

--- src/hooks/useDraggableObject.tsx
+++ src/hooks/useDraggableObject.tsx
@@ -11,26 +11,20 @@ function useDraggableObject(props: useDraggableObjectProps) {
 	const { presenter, setPresenter, editedSlideRef } = useContext(PresenterContext)
 	const size = window.innerHeight
 	const scale = (1080 / (size - 205)) * 1.2
-	let basePosition = { x: 0, y: 0 }
+	let obj = presenter.presentation.slides
+		.find((slide) => slide.id === props.slideId)
+		.objects.find((object) => object.id === props.elementId)
+	let baseObjPosition = { x: 0, y: 0 }
+	let baseMousePosition = { x: 0, y: 0 }
 	function moving(e) {
-		let obj = presenter.presentation.slides
-			.find((slide) => slide.id === props.slideId)
-			.objects.find((object) => object.id === props.elementId)
-		const rect = editedSlideRef.current.getBoundingClientRect()
-		const dx = (e.clientX - basePosition.x - rect.x) * scale - obj.baseState.x
-		const dy = (e.clientY - basePosition.y - rect.y) * scale - obj.baseState.y
-		console.log(
-			basePosition.x,
-			basePosition.y,
-			(e.clientX - basePosition.x - rect.x) * scale - obj.baseState.x,
-			(e.clientY - basePosition.y - rect.y) * scale - obj.baseState.y,
-		)
+		const dx = (e.clientX - baseMousePosition.x)
+		const dy = (e.clientY - baseMousePosition.y )
 		obj = {
 			...obj,
 			baseState: {
 				...obj.baseState,
-				x: obj.baseState.x + dx,
-				y: obj.baseState.y + dy,
+				x: baseObjPosition.x + dx * scale,
+				y: baseObjPosition.y + dy * scale,
 			},
 		}
 		const slideIndex = presenter.presentation.slides.findIndex(
@@ -63,7 +57,8 @@ function useDraggableObject(props: useDraggableObjectProps) {
 		props.elementRef.current.addEventListener('mousedown', startMoving)
 	}
 	function startMoving(e) {
-		basePosition = { x: e.clientX / scale, y: e.clientY / scale }
+		baseMousePosition = { x: e.clientX, y: e.clientY }
+		baseObjPosition = { x: obj.baseState.x, y: obj.baseState.y}
 		props.elementRef.current.removeEventListener('mousedown', startMoving)
 		editedSlideRef.current.addEventListener('mousemove', moving)
 		editedSlideRef.current.addEventListener('mouseup', stopMoving)
